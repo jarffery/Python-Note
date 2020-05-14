ee111dd7e8f677417bda3e10000d0e686575e0a6
jarffery <jerrylee0128@gmail.com>
2020-05-13 21:22:15 -0700

    updates

--- my script/wiki_spider/wiki_school.py
+++ my script/wiki_spider/wiki_school.py
@@ -4,7 +4,7 @@ import json
 import re
 from bs4 import BeautifulSoup
 import pandas as pd
-import googlemaps
+# import googlemaps
 from datetime import datetime
 
 
@@ -23,16 +23,38 @@ def wiki_search(school_name):
     return information
 
 def google_map_search(school_name):
+    school_dict = dict()
+    url_json = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="
     query = school_name
     query = query.replace(' ', '+')
-    gmaps = googlemaps.Client(key='AIzaSyA2p0RV-5We0ZvFSu1rGd_IPe82R32zld0')
-    geocode_result = gmaps.geocode(school_name)
+    my_key = '&key=AIzaSyCpMizkSRRC72cymY_75vIvXU88M9at_Fo'
+    text_search = url_json + query + my_key
+    text_search_result = requests.get(text_search)
+    try:
+        place_id = text_search_result.json()["results"][0]["place_id"]
+        address = text_search_result.json()["results"][0]["formatted_address"]
+        zip_code = re.search('\d{5}(-\d{4})?', address).group(0)
+        school_dict.update(
+            {school_name:
+                {"place_id":place_id, 
+                "address":address,
+                "zip_code":zip_code,
+                }})
+    except (RuntimeError, TypeError, NameError, AttributeError):
+        pass
+    url_detail = "https://maps.googleapis.com/maps/api/place/details/json?"
+    place_id_result = "place_id=" + school_dict[school_name]["place_id"]
+    Field = "&fields=name,formatted_phone_number"
+    detail_search = url_detail + place_id_result +Field + my_key
     
-for i in pd_school:
-    str(i).replace(" ","_")
+    try:
+        detail_search_result = requests.get(detail_search)
+        phone = detail_search_result.json()["result"]["formatted_phone_number"]
+        school_dict[school_name].update({"phone": phone})
+    except (RuntimeError, TypeError, NameError):
+        pass
+    
+    return text_search_result, detail_search_result, school_dict
 
 
-school_name = "Tuskegee_University"
-school_name = "Tuskegee University"
-google_search(school_name)
-wiki_search(school_name)
+x, y, z = google_map_search("Tuskegee University")
